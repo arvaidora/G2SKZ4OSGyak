0c47a7e4de83006afc566f0bfcacee3821a411ed
arvaidora <55945183+arvaidora@users.noreply.github.com>
2021-03-29 21:10:06 +0200

    0329

---
+++ G2SKZ4_0329/g2skz4_openclose.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <errno.h>
+
+int main(void) {
+    int fd; 
+    char *buf[100];
+    int len;
+
+    if ((fd = open("G2SKZ4.txt", O_RDWR)) < 0) {
+        perror("open hiba");
+        return -1;
+    }
+
+    if ((len = read(fd, buf, sizeof(buf))) != 0) {
+        if (len == -1 && errno != EINTR) {
+            perror("read hiba");
+            return -1;
+        }
+        int index;
+        for (index = 0; index < sizeof(buf) && buf[index] != EOF; index++);
+        buf[index] = '\0';
+        printf("Beolvasva: %d byte\nTartalma: %s\n", len, buf);
+    }
+
+    if (lseek(fd, 0, SEEK_SET) < 0) {
+        perror("lseek hiba");
+        return -1;
+    }
+    printf("Pozicio a fajl elejere mozgatva\n");
+
+    if ((len = write(fd, "Hello World", 11)) != 11) {
+        perror("write hiba");
+        return -1;
+    }
+    printf("%d byte kiírva\n", len);
+
+    close(fd);
+    return 0;
+}
